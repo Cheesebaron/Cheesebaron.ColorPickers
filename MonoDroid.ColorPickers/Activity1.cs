de4f4daf01aef709c436c3f1c0b4959252eee659
Tomasz Cielecki <tomasz@ostebaronen.dk>
2012-06-24 21:39:54 +0200

    First sample.

---
+++ MonoDroid.ColorPickers/Activity1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Android.App;
+using Android.Graphics;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MonoDroid.ColorPickers
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var colorPicker = new ColorPickerDialog(this, Color.Azure);
+            colorPicker.ColorChanged += (sender, args) => Console.WriteLine("Color changed: {0}", args.Color);
+
+            var colorPickerAlpha = new ColorPickerDialog(this, Color.Aqua);
+            colorPickerAlpha.ColorChanged += (sender, args) => Console.WriteLine("Color changed: {0}", args.Color);
+            colorPickerAlpha.AlphaSliderVisible = true;
+
+            var ll = new LinearLayout(this)
+                         {
+                             LayoutParameters = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FillParent,
+                                                                              ViewGroup.LayoutParams.FillParent)
+                         };
+
+            var btColorPicker = new Button(this) { Text = "Show Color Picker" };
+            btColorPicker.Click += (sender, args) => colorPicker.Show();
+
+            var btColorPickerAlpha = new Button(this) { Text = "Show Color Picker (Alpha)" };
+            btColorPickerAlpha.Click += (sender, args) => colorPickerAlpha.Show();
+
+            ll.AddView(btColorPicker);
+            ll.AddView(btColorPickerAlpha);
+            SetContentView(ll);
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -2,3 +2,11 @@ MonoDroid.ColorPickers
 ======================
 
 Sample library with ports of different Color Picker implementations.
+
+At the moment the only sample is the very nice Color Picker by [attenzione, android-ColorPickerPreference](https://github.com/attenzione/android-ColorPickerPreference)
+
+I plan to port the Simple round color picker from Android SDK samples as well pretty soon.
+
+License
+=======
+The license is [Apache 2.0](http://www.apache.org/licenses/LICENSE-2.0)
